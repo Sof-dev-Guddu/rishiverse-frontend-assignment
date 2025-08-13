c7ccc0d84a61b25401385afb2f9be81033473558
Sof-dev-Guddu <153995699+Sof-dev-Guddu@users.noreply.github.com>
2025-08-13 17:37:00 +0530

    fix: remove next/font to fix Vercel build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/header/Header";
 import { Toaster } from "@/components/ui/sonner";
 import AddUpdateStudentDialogWithRedux from "@/components/with-redux-wrapper/AddUpdateStudentDialogWithRedux";
-import DeleteAlertDialogWithRedux from "@/components/with-redux-wrapper/DeleteAlertDialogWithRedux";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Rishiverse",
   description: "Frontend challenge with JWT auth",
-
 };
 
 export default function RootLayout({
@@ -29,17 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className=" min-h-screen ">
-          <Header/>
+      <body className="font-sans antialiased">
+        <div className="min-h-screen">
+          <Header />
           {children}
-          <Toaster/>
-          <AddUpdateStudentDialogWithRedux/>
-          
-         </div>
-       
+          <Toaster />
+          <AddUpdateStudentDialogWithRedux />
+        </div>
       </body>
     </html>
   );
